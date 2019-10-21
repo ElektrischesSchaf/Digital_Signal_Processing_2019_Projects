876ce9c4eaa3636fc2a7a3b1e9e49c315d396557
ElektrischesSchaf <vertigo1958psycho1960@gmail.com>
2019-10-21 14:03:36 +0800

    recursion

--- Project_1/matlab.m
+++ Project_1/matlab.m
@@ -1,7 +1,8 @@
-n=20
+n=20;
 [x,t]=func1(n);
-scatter(t,x);
-
+scatter(t, x);
+output=func2(x, n);
+%fac_n=fac(x, n);
 
 function [signal,timestamp]=func1(n)
     signal=[]
@@ -19,5 +20,28 @@ function [signal,timestamp]=func1(n)
         signal=[signal temp]
         timestamp=[timestamp i]
         %x[i]=signal
+    end
+end
+
+function y=func2(x, n)
+    fprintf(' x(n)=%d',x(n) )
+    if n<3
+        fprintf(' n<2 \n')
+        y=0;
+    elseif n==0
+        y=0;
+    else
+        fprintf(1, '  in resursion  ')
+        fprintf('n= %d \n' ,n)
+        y=1.8.*cos(3.14/16).*func2(x, n-1)-0.81.*func2(x, n-2)+x(n)+0.5.*x(n-1);
+    end
+end
+function fn = fac(x, n)
+    haha=x(n)
+    n = floor(n);
+    if (n > 1)
+        fn = n * fac(x, n-1);
+    else
+        fn = 1;
     end
 end

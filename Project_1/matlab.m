c9a61bc80884b74719d616101bc66cb7e3964e08
ElektrischesSchaf <vertigo1958psycho1960@gmail.com>
2019-10-21 21:03:08 +0800

    project 1

--- Project_1/matlab.m
+++ Project_1/matlab.m
@@ -1,57 +1,77 @@
 n=20;
 [x,t]=func1(n);
-scatter(t, x);
-%fac_n=fac(x, n);
 outputs=[]
-
-output=func2(x, n); % return y[n]
-%%
+%output=func2(x, n); % return y[n]
 for i=1:20
     outputs(i)=func2(x, i); % see all y[n] from n=1 to n=20
 end
-%%
+
+[x2, t2]=func3(100) % plot range of x from -10 to 100
+
+% use matlab filter
 B=[1, 0.5]
 A=[1, -1.8*cos(3.14/16), 0.81]
-filter_output=filter(B, A, x);
+filter_output=filter(B, A, x2);
 
-function [signal,timestamp]=func1(n)
+scatter(t, x); % plot for question 1 
+scatter(t, outputs); % response from difference equation
+%scatter(t2, filter_output); % response from filter function
+
+%generate x from 0 to 20
+function [signal,timestamp]=func1(n) 
     signal=[]
     timestamp=[];
-    if n<0
-        return 
-    end    
+    if n<1
+        return
+    end
+    
     if n>20
         return
     end
     
     for i=1:n
-        temp=(0.9)^i
-        signal=[signal temp]
-        timestamp=[timestamp i]
-        %x[i]=signal
+        if i==0
+            temp=0
+            signal=[signal temp]
+            timestamp=[timestamp i]
+        else
+            temp=(0.9)^i
+            signal=[signal temp]
+            timestamp=[timestamp i]
+        end
     end
 end
 
+% use resursion to calculate y[n]
 function y=func2(x, n)
     fprintf(' n= %d ' ,n)
-    fprintf(' x(n)=%d ',x(n) )
-    if n<3
-        fprintf(' n<3 ');        
+    
+    if n<2
+        fprintf(' n<2 ');        
         y=0;
-    %elseif n<1
-    %    x(n)=0
-    %    y=0;
     else
+        fprintf(' x(n)=%d ',x(n) )
         fprintf('  in resursion  \n') 
         y=1.8.*cos(3.14/16).*func2(x, n-1)-0.81.*func2(x, n-2)+x(n)+0.5.*x(n-1);
     end
 end
-function fn = fac(x, n)
-    haha=x(n)
-    n = floor(n);
-    if (n > 1)
-        fn = n * fac(x, n-1);
-    else
-        fn = 1;
+
+% generate x from -10 to 100
+function [signal,timestamp]=func3(n) 
+    signal=[]
+    timestamp=[];
+    
+    for i=-10:n
+        if i<0
+            temp=0
+            signal=[signal temp]
+            timestamp=[timestamp i]
+        elseif i>100
+            return
+        else
+        temp=(0.9)^i
+        signal=[signal temp]
+        timestamp=[timestamp i]
+        end
     end
 end
